aebf413c2e0f23d8efd78b35c90f286f64b5afe2
duxinyues <yongyuan253015@gmail.com>
2022-11-10 09:46:25 +0800

    websocket

---
+++ ws.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: duxinyues yongyuan253015@gmail.com
+ * @Date: 2022-11-10 09:03:03
+ * @LastEditors: duxinyues yongyuan253015@gmail.com
+ * @LastEditTime: 2022-11-10 09:12:17
+ * @FilePath: \express-ui-api\ws.js
+ * @Description: 
+ * Copyright (c) 2022 by duxinyues email: yongyuan253015@gmail.com, All Rights Reserved.
+ */
+const express = require("express");
+const http = require("http");
+const Ws = require("ws").Server;
+const app = express();
+
+const server = http.createServer(app);
+
+server.listen(3000);
+app.use(express.static('www'));
+
+let wsServer = new Ws({server});
+
+wsServer.on("connection",function(socket){
+    console.log("连接成功");
+    socket.on("message",msg=>{
+        console.log("客户端信息",msg)
+        socket.send("服务器返回")
+    })
+})
