4b684af4e66eb92f99d4347c0d257368896b3a28
duxinyues <yongyuan253015@gmail.com>
2022-11-11 18:21:50 +0800

    websocket

--- ws.js
+++ ws.js
@@ -2,7 +2,7 @@
  * @Author: duxinyues yongyuan253015@gmail.com
  * @Date: 2022-11-10 09:03:03
  * @LastEditors: duxinyues yongyuan253015@gmail.com
- * @LastEditTime: 2022-11-10 09:12:17
+ * @LastEditTime: 2022-11-11 16:24:41
  * @FilePath: \express-ui-api\ws.js
  * @Description: 
  * Copyright (c) 2022 by duxinyues email: yongyuan253015@gmail.com, All Rights Reserved.
@@ -23,6 +23,6 @@ wsServer.on("connection",function(socket){
     console.log("连接成功");
     socket.on("message",msg=>{
         console.log("客户端信息",msg)
-        socket.send("服务器返回")
+        socket.send("客户端发送的信息："+msg+","+new Date().getTime())
     })
 })
