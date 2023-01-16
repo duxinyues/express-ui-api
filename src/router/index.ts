6baf0f305f7cb795a1c72374c56540bdd251777d
duxinyues <yongyuan253015@gmail.com>
2023-01-16 21:26:44 +0800

    feat: 文件上传

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import fs from "fs";
+import { rename } from "node:fs";
 import secret from "../config";
 import * as mysql from "mysql2";
 import * as jwt from "jsonwebtoken";
@@ -10,8 +10,22 @@ import { connection } from "../utils/mysql";
 import { Request, Response } from "express";
 import { createMathExpr } from "svg-captcha";
 
+let path = require("path");
+
 let generateVerify: number; // 保存验证
 let expiresIn = 60000; // 过期时间
+function verifyToken(req, res) {
+  try {
+    let accessToken = req.get("Authorization") as string;
+    if (accessToken.indexOf("Bearer") >= 0) {
+      accessToken = accessToken.replace("Bearer", "");
+    }
+    jwt.verify(accessToken, secret.jwtSecret);
+  } catch (error) {
+    res.status(401).end();
+    return res.json({ code: 401, mes: "暂无权限" }).end();
+  }
+}
 /**
  * @typedef Error
  * @property {string} code.required
@@ -43,62 +57,61 @@ let expiresIn = 60000; // 过期时间
  */
 const login = async (req: Request, res: Response) => {
   const { username, password } = req.body;
-  let sql: string =
-  `select * from users where username='${username}'`;
-connection.query(sql, async function (err, data: any) {
-  if (data.length == 0) {
-    await res.json({
-      success: false,
-      data: { message: Message[1] },
-    });
-  } else {
-    if (
-      createHash("md5").update(password).digest("hex") == data[0].password
-    ) {
-      const accessToken = jwt.sign(
-        {
-          accountId: data[0].id,
-        },
-        secret.jwtSecret,
-        { expiresIn }
-      );
-      if (username === "admin") {
-        await res.json({
-          success: true,
-          data: {
-            message: Message[2],
-            username,
-            // 这里模拟角色，根据自己需求修改
-            roles: ["admin"],
-            accessToken,
-            // 这里模拟刷新token，根据自己需求修改
-            refreshToken: "eyJhbGciOiJIUzUxMiJ9.adminRefresh",
-            expires: new Date(new Date()).getTime() + expiresIn,
-          },
-        });
-      } else {
-        await res.json({
-          success: true,
-          data: {
-            message: Message[2],
-            username,
-            // 这里模拟角色，根据自己需求修改
-            roles: ["common"],
-            accessToken,
-            // 这里模拟刷新token，根据自己需求修改
-            refreshToken: "eyJhbGciOiJIUzUxMiJ9.adminRefresh",
-            expires: new Date(new Date()).getTime() + expiresIn,
-          },
-        });
-      }
-    } else {
+  let sql: string = `select * from users where username='${username}'`;
+  connection.query(sql, async function (err, data: any) {
+    if (data.length == 0) {
       await res.json({
         success: false,
-        data: { message: Message[3] },
+        data: { message: Message[1] },
       });
+    } else {
+      if (
+        createHash("md5").update(password).digest("hex") == data[0].password
+      ) {
+        const accessToken = jwt.sign(
+          {
+            accountId: data[0].id,
+          },
+          secret.jwtSecret,
+          { expiresIn }
+        );
+        if (username === "admin") {
+          await res.json({
+            success: true,
+            data: {
+              message: Message[2],
+              username,
+              // 这里模拟角色，根据自己需求修改
+              roles: ["admin"],
+              accessToken,
+              // 这里模拟刷新token，根据自己需求修改
+              refreshToken: "eyJhbGciOiJIUzUxMiJ9.adminRefresh",
+              expires: new Date(new Date()).getTime() + expiresIn,
+            },
+          });
+        } else {
+          await res.json({
+            success: true,
+            data: {
+              message: Message[2],
+              username,
+              // 这里模拟角色，根据自己需求修改
+              roles: ["common"],
+              accessToken,
+              // 这里模拟刷新token，根据自己需求修改
+              refreshToken: "eyJhbGciOiJIUzUxMiJ9.adminRefresh",
+              expires: new Date(new Date()).getTime() + expiresIn,
+            },
+          });
+        }
+      } else {
+        await res.json({
+          success: false,
+          data: { message: Message[3] },
+        });
+      }
     }
-  }
-});
+  });
 };
 /**
  * @typedef Register
@@ -120,7 +133,6 @@ connection.query(sql, async function (err, data: any) {
  */
 const register = async (req: Request, res: Response) => {
   const { username, password } = req.body;
-  console.log("000",req.body)
   if (password.length < 6) {
     return res.json({
       success: false,
@@ -180,17 +192,10 @@ const register = async (req: Request, res: Response) => {
  */
 
 const updateList = async (req: Request, res: Response) => {
-  console.log("更新",req.body,req.params)
+  console.log("更新", req.body, req.params);
   const { id } = req.params;
   const { username } = req.body;
-  let payload = null;
-  try {
-    const authorizationHeader = req.get("Authorization") as string;
-    const accessToken = authorizationHeader.substr("Bearer ".length);
-    payload = jwt.verify(accessToken, secret.jwtSecret);
-  } catch (error) {
-    return res.status(401).end();
-  }
+  verifyToken(req, res);
   let modifySql: string = "UPDATE users SET username = ? WHERE id = ?";
   let sql: string = "select * from users where id=" + id;
   connection.query(sql, function (err, data) {
@@ -214,5 +219,119 @@ const updateList = async (req: Request, res: Response) => {
     });
   });
 };
+/**
+ * @typedef SearchPage
+ * @property {integer} page.required - 第几页 - eg: 1
+ * @property {integer} size.required - 数据量（条）- eg: 5
+ */
 
-export { login, register,updateList };
+/**
+ * @route POST /searchPage
+ * @param {SearchPage.model} point.body.required - the new point
+ * @produces application/json application/xml
+ * @consumes application/json application/xml
+ * @summary 分页查询
+ * @group 用户管理相关
+ * @returns {Response.model} 200
+ * @returns {Array.<SearchPage>} SearchPage
+ * @headers {integer} 200.X-Rate-Limit
+ * @headers {string} 200.X-Expires-After
+ * @security JWT
+ */
+
+const searchPage = async (req: Request, res: Response) => {
+  const { page, size } = req.body;
+  verifyToken(req, res);
+  let sql: string =
+    "select * from users limit " + size + " offset " + size * (page - 1);
+  connection.query(sql, async function (err, data) {
+    if (err) {
+      Logger.error(err);
+    } else {
+      await res.json({
+        success: true,
+        data,
+      });
+    }
+  });
+};
+/**
+ * @typedef SearchVague
+ * @property {string} username.required - 用户名  - eg: admin
+ */
+
+/**
+ * @route POST /searchVague
+ * @param {SearchVague.model} point.body.required - the new point
+ * @produces application/json application/xml
+ * @consumes application/json application/xml
+ * @summary 模糊查询
+ * @group 用户管理相关
+ * @returns {Response.model} 200
+ * @returns {Array.<SearchVague>} SearchVague
+ * @headers {integer} 200.X-Rate-Limit
+ * @headers {string} 200.X-Expires-After
+ * @security JWT
+ */
+
+const searchVague = async (req: Request, res: Response) => {
+  console.log("模糊查询", req.body);
+  const { username } = req.body;
+  verifyToken(req, res);
+  if (username === "" || username === null)
+    return res.json({
+      success: false,
+      data: { message: Message[9] },
+    });
+  let sql: string = "select * from users";
+  sql += " WHERE username LIKE " + mysql.escape("%" + username + "%");
+  connection.query(sql, function (err, data) {
+    connection.query(sql, async function (err) {
+      if (err) {
+        Logger.error(err);
+      } else {
+        await res.json({
+          success: true,
+          data,
+        });
+      }
+    });
+  });
+};
+// 多文件上传
+const upload = async (req: any, res: Response) => {
+  // 文件存放地址
+  const des_filesPath = "./public/uploads/";
+  let filesLength: number = req.files.length;
+  let result = [];
+  let filesInfo: any = {};
+  if (filesLength === 0) {
+    res.render("error", { mes: "文件不能为空" });
+    return;
+  }
+  req.files.forEach((element) => {
+    rename(
+      des_filesPath + element.filename,
+      des_filesPath + element.originalname,
+      (err) => {
+        if (err) {
+          console.log("文件重名错误", err);
+        }
+      }
+    );
+    filesInfo.mimetype = element.mimetype;
+    filesInfo.originalname = element.originalname;
+    filesInfo.size = element.size;
+    filesInfo.path = des_filesPath + element.originalname;
+    result.push(filesInfo);
+  });
+
+  console.log("结果", result);
+  res.set({ "content-type": "application/json;charset=uft-8" });
+  res.send({
+    success: true,
+    mes: "上传成功",
+    data: result,
+  });
+};
+export { login, register, updateList, searchPage, searchVague, upload };
--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,19 @@ import config from "./config";
 import { user } from "./models/mysql";
 import Logger from "./loaders/logger";
 import { queryTable } from "./utils/mysql";
-import { login, register, updateList } from "./router";
+import * as multer from "multer";
+import {
+  login,
+  register,
+  updateList,
+  searchPage,
+  searchVague,
+  upload,
+} from "./router";
 const expressSwagger = require("express-swagger-generator")(app);
 
 expressSwagger(config.options);
-// queryTable(user);
+queryTable(user);
 
 app.post("/login", (req, res) => {
   login(req, res);
@@ -16,13 +24,24 @@ app.post("/register", (req, res) => {
   register(req, res);
 });
 app.put("/updateList/:id", (req, res) => {
-    updateList(req, res);
-  });
+  updateList(req, res);
+});
+app.post("/searchPage", (req, res) => {
+  searchPage(req, res);
+});
+app.post("/searchVague", (req, res) => {
+  searchVague(req, res);
+});
+// 新建存放临时文件的文件夹
+const upload_tmp = multer({ dest: "./public/uploads" });
+app.post("/upload", upload_tmp.any(), (req, res) => {
+  upload(req, res);
+});
 app
-  .listen(config.port, () => {
+  .listen(8098, () => {
     Logger.info(`
     ################################################
-    🛡️ 文档地址: http://localhost:${config.port} 🛡️
+    🛡️ 文档地址: http://localhost:8098 🛡️
     ################################################
   `);
   })
