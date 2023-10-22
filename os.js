49f576b72ccfd43cc7b74cf89d4db5e59dcc9e3f
duxinyues <weiyy26445@yunrong.cn>
2023-10-22 21:54:12 +0800

    fix

--- os.js
+++ os.js
@@ -2,8 +2,9 @@
  * @Author: duxinyues weiyy26445@yunrong.cn
  * @Date: 2023-10-22 21:47:25
  * @LastEditors: duxinyues weiyy26445@yunrong.cn
- * @LastEditTime: 2023-10-22 21:47:26
+ * @LastEditTime: 2023-10-22 21:54:06
  * @FilePath: /node/os.js
  * @Description: 
  * Copyright (c) 2023 by ${duxinyues} email: ${weiyy26445@yunrong.cn}, All Rights Reserved.
  */
+const os = require('os')
