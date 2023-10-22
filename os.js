08e2ef708ec186f8d513bf77b0f894e692a66ce3
duxinyues <yongyuan253015@gmail.com>
2023-10-22 22:00:07 +0800

    fix:系统文件操作

--- os.js
+++ os.js
@@ -1,10 +1,12 @@
 /*
  * @Author: duxinyues weiyy26445@yunrong.cn
  * @Date: 2023-10-22 21:47:25
- * @LastEditors: duxinyues weiyy26445@yunrong.cn
- * @LastEditTime: 2023-10-22 21:54:06
+ * @LastEditors: duxinyues yongyuan253015@gmail.com
+ * @LastEditTime: 2023-10-22 21:57:42
  * @FilePath: /node/os.js
  * @Description: 
  * Copyright (c) 2023 by ${duxinyues} email: ${weiyy26445@yunrong.cn}, All Rights Reserved.
  */
-const os = require('os')
+const os = require('os');
+
+console.log("os.freemem()",os.freemem())
